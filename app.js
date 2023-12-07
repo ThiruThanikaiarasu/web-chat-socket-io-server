138a44fb1ee07fe898710ad7c0be4094df00cc09
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-12-07 10:18:45 +0530

    Connection Established to MongoDB and Socket.io

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = 3500
 
+const rooms = ['general', 'tech', 'finance', 'crypto'] 
 
-app.listen(PORT, console.log(`Server is running at http://localhost:${PORT}`))
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { error } = require('console')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+// Establishing Connection to MongoDB
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+
+db.on( "error", (error) => console.log(error))
+db.once( "open", () => console.log("Connection Established"))
+
+// Establishing Connection to Socket
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {
+    cors : {
+        origin : 'http://localhost:5173',
+        methods : ['GET', 'POST']
+    }
+})
+
+server.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
